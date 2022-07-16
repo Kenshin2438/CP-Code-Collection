b060e679b9ebd08129f960e1efba929ec2270ade
Kenshin2438 <2953957899@qq.com>
2022-07-16 15:54:32 +0800

    update chtholly_tree

--- template/data_structure/chtholly_tree.hpp
+++ template/data_structure/chtholly_tree.hpp
@@ -1,28 +1,19 @@
 #include "../main.hpp"
 
 struct ODT {
-  map<ll, ll> mp;
+  map<int, int> mp;
 
   ODT() = default;
-  ODT(ll _, ll unit) { mp[_ - 1] = unit; }
+  ODT(int _, int unit) { mp[_ - 1] = unit; }
   ~ODT() = default;
 
-  void split(ll x) { mp[x] = prev(mp.upper_bound(x))->second; }
-  void assign(ll l, ll r, ll v) {  // assign [l, r), value v
+  void split(int x) { mp[x] = prev(mp.upper_bound(x))->second; }
+  int get(int x) { return prev(mp.upper_bound(x))->second; }
+  void assign(int l, int r, int v) {  // assign [l, r), value v
     split(l), split(r);
     auto it = mp.find(l);
     while (it->first != r) it = mp.erase(it);
-    mp[l] = v;
-  }
-  ll query(ll l, ll r) {  // query [l, r)
-    split(l), split(r);
-    auto it = mp.find(l);
-    ll res = 0;
-    while (it->first != r) {
-      auto nex = next(it);
-      res += it->second * (nex->first - it->first);
-      it = nex;
-    }
-    return res;
+    if (get(l - 1) != v) mp[l] = v;
+    if (it->second == v) mp.erase(it);
   }
 };
