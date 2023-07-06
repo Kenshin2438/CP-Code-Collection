33ae06623254b3bce3562b8e666db4901e4cdc05
Kenshin2438 <2953957899@qq.com>
2023-07-06 16:06:37 +0800

    Replace ms-vscode.cpptools by clangd

--- .vscode/acm-run.ps1
+++ .vscode/acm-run.ps1
@@ -18,13 +18,13 @@ $compilerArgs = @(
   "-Wall",
   "-Wextra",
   "-Wfloat-equal",
-  "-Wl,--stack=536870912",
+  "-Wl,--stack=268435456",
   "-O2",
   "-DKENSHIN"
 )
 
 if ((-not $existed) -or ($srcTime -gt $exeTime)) {
-  & g++ @compilerArgs
+  & sccache g++ @compilerArgs
 
   if ($LASTEXITCODE -ne 0) {
     Write-Host "$source Build Failed`n" -ForegroundColor Red
